b746e113c5bb6518cbdd77889e043bb9d0017fc1
Karthika V <karthika.v2024aids@sece.ac.in>
2026-07-24 19:00:44 +0530

    Update page.tsx

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,10 +1,3 @@
-import { headers } from 'next/headers';
-import { App } from '@/components/app/app';
-import { getAppConfig } from '@/lib/utils';
-
-export default async function Page() {
-  const hdrs = await headers();
-  const appConfig = await getAppConfig(hdrs);
-
-  return <App appConfig={appConfig} />;
+export default function Page() {
+  return <h1>NEURA COP Test</h1>;
 }
